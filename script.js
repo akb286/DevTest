f1b22ebef9082d8e9c75912565f1caa370f28791
Alicia Baker <aliciabaker314@gmail.com>
2018-12-15 13:05:39 -0700

    working on json

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const getData = () => {
-fetch("file.json")
-	.then(function(response) {
-		return response.json();
-	})
-	.then(function(data) {
-		console.log(data);
-	});
+	return fetch('http://jsonplaceholder.typicode.com/users')
+	.then(res => res.json())
+	.then(data => console.log(data))
 }
+
+const data = {
+	title: "wow",
+	body: "users",
+}
+const newData = post => {
+	const options = {
+		method: 'POST',
+		body: JSON.stringify(post),
+		headers: new Headers ({
+			'Content-Type': 'application/json'
+		})
+	}
+
+	return fetch('http://jsonplaceholder.typicode.com/users', options)
+		.then(res => res.json())
+		.then(res => console.log(res))
+}
+
+newData(data)
