a1f8321f85faf586f250fbd3fd446a69ae4cca1e
Alicia Baker <aliciabaker314@gmail.com>
2018-12-15 14:03:01 -0700

    photo is appearing

--- script.js
+++ script.js
@@ -1,25 +1,59 @@
-const getData = () => {
-	return fetch('http://jsonplaceholder.typicode.com/users')
-	.then(res => res.json())
-	.then(data => console.log(data))
+function createNode(element){
+	return document.createElement(element);
 }
 
-const data = {
-	title: "wow",
-	body: "users",
-}
-const newData = post => {
-	const options = {
-		method: 'POST',
-		body: JSON.stringify(post),
-		headers: new Headers ({
-			'Content-Type': 'application/json'
-		})
-	}
-
-	return fetch('http://jsonplaceholder.typicode.com/users', options)
-		.then(res => res.json())
-		.then(res => console.log(res))
+function append(parent, el) {
+	return parent.appendChild(el);
 }
 
-newData(data)
+const ul = document.getElementById("users");
+const url = 'https://randomuser.me/api/?results=5'
+
+fetch(url)
+.then((resp) => resp.json())
+.then(function(data){
+	let users = data.results;
+
+	return users.map(function(user){
+			let li = createNode('li'),
+			img = createNode('img'),
+			span = createNode('span');
+
+			img.src=user.picture.medium;
+			span.innerHtml=`${user.name.first} ${user.name.last}`;
+
+			append(li,img);
+			append(li,span);
+			append(ul,li);
+	})
+})
+
+.catch(function(error){
+	console.log(JSON.stringify(error))
+});
+
+// const getData = () => {
+// 	return fetch('http://jsonplaceholder.typicode.com/users')
+// 	.then(res => res.json())
+// 	.then(data => console.log(data))
+// }
+//
+// const data = {
+// 	title: "wow",
+// 	body: "users",
+// }
+// const newData = post => {
+// 	const options = {
+// 		method: 'POST',
+// 		body: JSON.stringify(post),
+// 		headers: new Headers ({
+// 			'Content-Type': 'application/json'
+// 		})
+// 	}
+//
+// 	return fetch('http://jsonplaceholder.typicode.com/users', options)
+// 		.then(res => res.json())
+// 		.then(res => console.log(res))
+// }
+//
+// newData(data)
